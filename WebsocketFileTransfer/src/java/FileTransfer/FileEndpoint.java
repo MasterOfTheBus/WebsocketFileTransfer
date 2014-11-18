3b4b167d9e88b4750cff23405d91e42ccbf35fe5
Sidney Ng <sidney.ng@mail.mcgill.ca>
2014-11-18 10:45:17 -0500

    pushing what i have now

--- WebsocketFileTransfer/src/java/FileTransfer/FileEndpoint.java
+++ WebsocketFileTransfer/src/java/FileTransfer/FileEndpoint.java
@@ -5,8 +5,14 @@
  */
 package FileTransfer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
 import javax.websocket.OnMessage;
 import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session;
 
 /**
  *
@@ -15,8 +21,33 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/transfer")
 public class FileEndpoint {
     
+    static int BUFFER_SIZE =  4096;
+    
     @OnMessage
-    public String echo(String message) {
-        return message;
+    public String sendFile(String message, Session session) {
+        if (message == null) {
+            return ("ERROR");
+        }
+        
+        // check if file exists
+        message = "C:\\cygwin\\home\\Lazy\\workspace\\Telecom\\WebsocketFileTransfer\\WebsocketFileTransfer\\send\\" + message;
+        File f = new File(message);
+        
+        // open file and send send as a binary message
+        try {
+            FileInputStream in = new FileInputStream(f);
+            OutputStream out = session.getBasicRemote().getSendStream();
+            byte[] bArray = new byte[BUFFER_SIZE];
+            
+            int read = in.read(bArray);
+            while (read != -1) {          
+                out.write(bArray);
+                read = in.read(bArray);
+            }
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+        
+        return ("File Sent");
     }
 }
